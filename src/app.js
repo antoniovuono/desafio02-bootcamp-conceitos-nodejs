e53b923b5ab22a06a8b84fa6c72a2262619248ed
Antonio Sampaio De Vuono <antoniosvuono@gmail.com>
2020-04-13 13:37:38 -0300

    Metodo POST criado

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
 
 const app = express();
+
 
 app.use(express.json());
 app.use(cors());
@@ -12,11 +13,20 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
-});
+    const { title, url, techs } = request.body;
+
+    const repositorie = { id: uuid(), title, url, techs };
+
+    repositories.push(repositorie);
+
+    return response.json(repositorie);
+
+  });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
